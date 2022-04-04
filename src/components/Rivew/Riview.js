15307a85f4ff06e3b8ad5ec9645cbb659d56db05
SMhamidAzad <hamidazad020@gmail.com>
2022-04-04 16:42:27 +0600

    riviews style added

--- src/components/Rivew/Riview.js
+++ src/components/Rivew/Riview.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-const Riview = ({riview}) => {
-    const {name,picture,comment,rating}=riview;
+const Riview = ({ riview }) => {
+    const { name, picture, comment, rating } = riview;
     return (
-        <div>
-            <img style={{width: '60px',height: '60px', borderRadius: '50%'}} className='' src={picture} alt="" />
+        <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', borderRadius: '10px', width: '47%'}} className="m-3 p-3">
+            <div className='d-flex justify-content-between mb-3 '>
+                <div className='d-flex'>
+                    <img style={{ width: '60px', height: '60px', borderRadius: '50%' }} className='' src={picture} alt="" />
+                    <h6 className='d-flex align-items-center ms-2'>{name}</h6>
+                </div>
+                <div className='d-flex align-items-center'>
+                    <p className='text-warning fs-4'>{rating}</p>
+                </div>
+            </div>
+
             <p>{comment}</p>
-            <h6>{name}</h6>
-            <p>Rating: {rating}</p>
+
         </div>
     );
 };
--- src/components/Riviews/Riviews.js
+++ src/components/Riviews/Riviews.js
@@ -2,21 +2,19 @@ import useReviews from '../../hooks/useReviews';
 import Riview from '../Rivew/Riview';
 
 const Riviews = () => {
-    // const [reviews,setReviews] = useState([]);
-    // useEffect(()=>{
-    //     fetch('reviews.json')
-    //     .then(res => res.json())
-    //     .then(data => setReviews(data));
-    // },[]);
+   
     const [reviews, setReviews] = useReviews();
     return (
         <div className='container'>
-            {
+            <h2 style={{ color: '#040924'}} className='text-center my-3 fw-bolder'>Custommer Says</h2>
+           <div className='row row-cols-2'>
+           {
                 reviews.map(review => <Riview
                 key={review._id}
                 riview={review}
                 ></Riview>)
             }
+           </div>
         </div>
     );
 };
