d4ee516395afa41c6224e26c2e8262b1a8b9daf9
SMhamidAzad <hamidazad020@gmail.com>
2022-04-04 20:09:09 +0600

    blog page updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,22 +2,19 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div style={{ backgroundColor: '#F3F4F6', height: '88vh' }}>
+        <div style={{ backgroundColor: '#F3F4F6', position: 'absolute', height: '88vh', width: '100%'}} className="pt-5">
+            <p className='fs-2 text-center mt-4'>People want to know</p>
             <div className="container">
-               <div className="col">
                <div>
-                    <p className='fs-2 text-center'>People want to know</p>
+               <div>
                     <h4>What is Context API?</h4>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus doloremque in nobis, rerum eius eligendi cumque, minima, aperiam numquam beatae earum sapiente nemo quod culpa perspiciatis magnam corrupti animi reiciendis.</p>
+                    <p>Without passing props to every level, we can easily access data at different levels by using Context API. It solves the problem of props drilling. Suppose, we have 3 components A, B, C. Here, A is the parent component of B. B is the parent component of C. If we need to pass data from A to C, we can not pass data directly from A to B. Using props, we need to pass to data from A to B then C using props. On the other hand, using context API, we can dirctly pass data from A to C. It is a way for a React app to effectively produce global variables that can be passed around.</p>
                 </div>
                 <div>
-                    <h4>What is Symantic tag?</h4>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt harum, architecto necessitatibus error vero, velit dolore, itaque ea explicabo quasi similique soluta asperiores enim earum culpa neque delectus. Doloribus, deserunt!</p>
+                    <h4>What is Semantic tag?</h4>
+                    <p>A semantic tag is a tag that clearly gives a meaning to the content of a tag. Using semantic tag both browser and human understand the content of a tag. Example of some semantic tags are header, footer, article, form, table, section, aside etc. It has great  accessibility. When we make a page using semantic tag, it is easier to read. Every semantic tags are the indrution of the part of the code. When we use a header tag in website, anyone can easily understand it that this section is header part of website.</p>
                 </div>
-                <div>
-                    <h4>Inline vs Block vs Inline-Block</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio laboriosam modi dolores enim animi illum tempore quo ut nam corporis, assumenda veritatis voluptatem iste eos quaerat iusto delectus quidem eius!</p>
-                </div>
+
                </div>
             </div>
         </div>
