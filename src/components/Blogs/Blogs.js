9b63aeadc8f5373357323089d2954504ed1219e6
SMhamidAzad <hamidazad020@gmail.com>
2022-04-04 17:06:50 +0600

    about part added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is bllog compo</h1>
+        <div style={{ backgroundColor: '#F3F4F6', height: '88vh' }}>
+            <div className="container">
+               <div className="col">
+               <div>
+                    <p className='fs-2 text-center'>People want to know</p>
+                    <h4>What is Context API?</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus doloremque in nobis, rerum eius eligendi cumque, minima, aperiam numquam beatae earum sapiente nemo quod culpa perspiciatis magnam corrupti animi reiciendis.</p>
+                </div>
+                <div>
+                    <h4>What is Symantic tag?</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt harum, architecto necessitatibus error vero, velit dolore, itaque ea explicabo quasi similique soluta asperiores enim earum culpa neque delectus. Doloribus, deserunt!</p>
+                </div>
+                <div>
+                    <h4>Inline vs Block vs Inline-Block</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio laboriosam modi dolores enim animi illum tempore quo ut nam corporis, assumenda veritatis voluptatem iste eos quaerat iusto delectus quidem eius!</p>
+                </div>
+               </div>
+            </div>
         </div>
     );
 };
